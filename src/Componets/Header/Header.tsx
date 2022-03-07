384471e13749ff6f7d239d6661bcb888d40387cd
Kali20199 <49203126+Kali20199@users.noreply.github.com>
2022-03-07 06:23:04 +0200

    last

--- src/Componets/Header/Header.tsx
+++ src/Componets/Header/Header.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
+import { useNavigate } from 'react-router'
 
+import './Header.scss'
 function Header() {
+
+
+
+  let nav =  useNavigate()
+
   return (
-    <div>Header</div>
+    <header>
+    <Navbar bg="dark" expand="lg" variant='dark' collapseOnSelect >
+    <Container className=''>
+      <Navbar.Brand style={{cursor:'pointer'}} onClick={()=>{nav('../../')}}>Pro Shop</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+     
+        <Nav className="ml-auto header_items">
+          <Nav.Link href="#home"><i className='fas fa-shopping-cart'/> Cart</Nav.Link>
+          <Nav.Link href="#link"><i className='fas fa-user'/>  Sign In</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  </header>
   )
 }
 
